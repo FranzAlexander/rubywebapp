b17cbde603ba811c75653a2205eb5fa3b3dee050
FranzAlexander <alexbalcombe4@gmail.com>
2017-05-24 09:51:52 +0800

    Add Config Files

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  resources :contents
+  resources :courses
+  resources :users
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'courses/:id/contents', to: 'contents#index', as: 'course_contents'
+
+  get 'courses/:id/contents/new', to: 'contents#new', as: 'course_contents_new'
+
+  root to: 'home#new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
